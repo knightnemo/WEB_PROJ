6f80d3412a61a68c167c4262d0d7300bc3011cb8
Nemo Huang <knightnemo@Nemos-MacBook-Air.local>
2024-07-08 10:20:29 +0800

    fixed bug 1

--- doctor/src/main/scala/Impl/RegisterMessagePlanner.scala
+++ doctor/src/main/scala/Impl/RegisterMessagePlanner.scala
@@ -3,30 +3,39 @@ package Impl
 import cats.effect.IO
 import io.circe.generic.auto.*
 import Common.API.{PlanContext, Planner}
-import Common.DBAPI.{writeDB, *}
-import Common.Object.{ParameterList, SqlParameter}
+import Common.DBAPI.{writeDB, readDBBoolean}
+import Common.Object.SqlParameter
 import Common.ServiceUtils.schemaName
-import APIs.PatientAPI.PatientQueryMessage
-import cats.effect.IO
-import io.circe.generic.auto.*
+import APIs.DoctorAPI.DoctorInfo
 
-
-case class RegisterMessagePlanner(userName: String, password: String,override val planContext: PlanContext) extends Planner[String]:
+case class RegisterMessagePlanner(userName: String, password: String, bio: Option[String], override val planContext: PlanContext) extends Planner[String]:
   override def plan(using planContext: PlanContext): IO[String] = {
-    // Check if the user is already registered
-    val checkUserExists = readDBBoolean(s"SELECT EXISTS(SELECT 1 FROM ${schemaName}.user_name WHERE user_name = ?)",
-        List(SqlParameter("String", userName))
-      )
+    val checkUserExists = readDBBoolean(
+      s"SELECT EXISTS(SELECT 1 FROM ${schemaName}.user_name WHERE user_name = ?)",
+      List(SqlParameter("String", userName))
+    )
 
     checkUserExists.flatMap { exists =>
       if (exists) {
         IO.raiseError(new Exception("already registered"))
       } else {
-        writeDB(s"INSERT INTO ${schemaName}.user_name (user_name, password) VALUES (?, ?)",
-          List(SqlParameter("String", userName),
-               SqlParameter("String", password)
-          ))
+        for {
+          _ <- writeDB(
+            s"INSERT INTO ${schemaName}.user_name (user_name, password) VALUES (?, ?)",
+            List(SqlParameter("String", userName), SqlParameter("String", password))
+          )
+          _ <- writeDB(
+            s"""
+            INSERT INTO ${schemaName}.doctors 
+            (user_name, bio, followers, following, review_count) 
+            VALUES (?, ?, 0, 0, 0)
+            """,
+            List(
+              SqlParameter("String", userName),
+              SqlParameter("String", bio.getOrElse(""))
+            )
+          )
+        } yield "User registered successfully"
       }
     }
   }
-
